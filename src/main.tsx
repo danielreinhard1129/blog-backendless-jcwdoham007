c438c398e1fdf04e093964fef0ca304a77523513
danielreinhard1129 <danielreinhard1129@gmail.com>
2026-06-22 09:43:35 +0700

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import "./index.css";
+import Homepage from "./pages/Homepage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />,
+);
