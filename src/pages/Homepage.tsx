0747fc81bcc12506e29fe8a476ce174b0803b233
danielreinhard1129 <danielreinhard1129@gmail.com>
2026-06-23 10:10:56 +0700

    feat: add homepage

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,7 +1,15 @@
+import { Footer } from "@/components/Footer";
+import Grid from "@/components/Homepage/Grid";
+import Hero from "@/components/Homepage/Hero";
+import { Navbar } from "@/components/Navbar";
+
 function Homepage() {
   return (
-    <div>
-      <h1>Homepage</h1>
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <Hero />
+      <Grid />
+      <Footer />
     </div>
   );
 }
